4dbf1b536894643df147ef5f12cb9c709c865954
briansodenkin <briansodenkin@gmail.com>
2022-08-17 14:34:35 +0800

    CUshop

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const seedRouter = require("./routes/seedRoutes.js");
+const productRouter = require("./routes/productRoutes.js");
+const userRouter = require("./routes/userRoutes.js");
+const orderRouter = require("./routes/orderRoutes.js");
+const uploadRouter = require("./routes/uploadRoutes.js");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
+app.get("/api/keys/google", (req, res) => {
+  res.send({ key: process.env.GOOGLE_API_KEY || "" });
+});
+
+app.use("/api/upload", uploadRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
